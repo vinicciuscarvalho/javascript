72993c3e38fcf0cf085465223d6db7abada4d125
Vinicius Carvalho <119802393+vinicciuscarvalho@users.noreply.github.com>
2025-08-09 09:52:02 -0300

    update

---
+++ aula06/index.js
@@ -0,0 +1,23 @@
+/* Vamos entender Variáveis
+Variáveis são espaços na memória onde podemos armazenar valores.
+
+No JavaScript temos 3 palavras-chaves para declarar variáveis: var, let e const.
+*/
+
+//DECLARAÇÃO DE VARIÁVEIS
+var a,b,c;
+
+a = 2;
+b = 3;
+c = a + b;
+
+alert(c); // Exibe o valor de c, que é 5
+
+var nome, sobrenome, nomeCompleto;
+
+nome = "Vinicius";
+sobrenome = "Carvalho";
+nomeCompleto = nome + sobrenome;
+document.getElementById("texto").innerHTML = nomeCompleto
+
+ 
