afcddc706987635b48fde9f79289d20e843b9642
Vinicius Carvalho <119802393+vinicciuscarvalho@users.noreply.github.com>
2025-12-03 18:47:06 -0300

    update

--- aulas/aula05.js
+++ aulas/aula05.js
@@ -1,1 +1,11 @@
-/* >, >=, <, <=, ==, != */
+/*
+Operadores Relacionais: >, >=, <, <=, ==, != 
+
+Irão retornar o valor TRUE ou FALSE
+*/
+
+let num1=10
+let num2=5
+let num3=10
+
+console.log(num1 > num2)  // true
